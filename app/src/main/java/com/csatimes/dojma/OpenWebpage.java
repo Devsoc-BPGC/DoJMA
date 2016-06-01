475781fc065b19a5dcb394db96a8237a494b9774
Vikramaditya Kukreja <kukreja.vikramaditya@gmail.com>
2016-06-01 19:53:15 +0530

    Added WebView for Herald items

---
+++ app/src/main/java/com/csatimes/dojma/OpenWebpage.java
@@ -0,0 +1,138 @@
+package com.csatimes.dojma;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+/**
+ * This class handles the url passed from Herald on item click.
+ * It uses webView to load the url and also sets a title to the actionbar.
+ */
+public class OpenWebpage extends AppCompatActivity {
+    private WebView webView;
+    private ProgressBar pb;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_webpage);
+        webView = (WebView) findViewById(R.id.webView);
+        pb = (ProgressBar) findViewById(R.id.webPageProgressBar);
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("CSATimes");
+            actionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color
+                    .colorPrimaryDark)));
+        }
+
+
+        Intent intent = getIntent();
+        if (actionBar != null) actionBar.setTitle(intent.getStringExtra("TITLE"));
+
+
+        /**
+         * The following codes are for the webView's settings
+         */
+        webView.setInitialScale(1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().supportZoom();
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        //finally load webview
+        webView.loadUrl(intent.getStringExtra("URL"));
+        //in case user clicks on links within the webview
+        //because of the following code it will be handled inside webview
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                ActionBar actionBar = getSupportActionBar();
+                if (actionBar != null) actionBar.setTitle("Loading...");
+                view.loadUrl(url);
+                if (actionBar != null) actionBar.setTitle("CSATimes");
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                pb.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                pb.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                super.onReceivedHttpError(view, request, errorResponse);
+                Snackbar.make(view, "HTTP error from server! Try again later. If the problem " +
+                        "persists write" +
+                        " to us", Snackbar.LENGTH_LONG).show();
+            }
+
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                pb.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                if (actionBar != null)
+                    actionBar.setTitle(title);
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+            }
+        });
+
+
+    }
+
+    /**
+     * This method(overridden) handles onBack pressing.If webView has history it will go back
+     * otherwise it
+     * will return to calling class - Herald in this case
+     */
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null)
+                actionBar.setTitle("CSATimes");
+            return;
+        }
+        // Otherwise defer to system default behavior.
+        super.onBackPressed();
+    }
+}
