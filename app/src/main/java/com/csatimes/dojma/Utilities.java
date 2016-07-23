6ace3844e9d9f0a9757be4c4b90d7dfdfe999ecc
yash sharan <yashsharan.0805@gmail.com>
2016-07-23 13:00:20 +0530

    recommitted the files for link list view

--- app/src/main/java/com/csatimes/dojma/Utilities.java
+++ app/src/main/java/com/csatimes/dojma/Utilities.java
@@ -33,7 +33,7 @@ public class Utilities extends Fragment {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //reload my activity with permission granted or use the features what required the permission
                 } else {
-                    Toast.makeText(getActivity(), "The app was not allowed to write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
+                     Toast.makeText(getActivity(), "The app was not allowed to write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
                 }
             }
         }
