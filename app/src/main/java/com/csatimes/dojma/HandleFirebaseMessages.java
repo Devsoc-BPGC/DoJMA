ec516f3c3b79153824a8f7c4be375113dc6a423e
yash sharan <yashsharan.0805@gmail.com>
2016-08-02 21:55:05 +0530

    Added notification while App is open

--- app/src/main/java/com/csatimes/dojma/HandleFirebaseMessages.java
+++ app/src/main/java/com/csatimes/dojma/HandleFirebaseMessages.java
@@ -1,5 +1,11 @@
 package com.csatimes.dojma;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -9,7 +15,20 @@ public class HandleFirebaseMessages extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        Log.e("TAG", remoteMessage.getTtl() + " getttl");
-
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.dojma))
+                        .setSmallIcon(R.drawable.dojma)
+                        .setContentTitle(remoteMessage.getNotification().getTitle())
+                        .setContentText(remoteMessage.getNotification().getBody());
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, HomeActivity.class);
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(this,69,resultIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(6969, mBuilder.build());
     }
 }
