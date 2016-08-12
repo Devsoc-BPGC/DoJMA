bf4b1a1dcec455b1de99216690e5e662d1190dd5
Vikramaditya Kukreja <kukreja.vikramaditya@gmail.com>
2016-08-12 15:42:51 +0530

    Notification improvements in HandleFirebaseMessages

--- app/src/main/java/com/csatimes/dojma/HandleFirebaseMessages.java
+++ app/src/main/java/com/csatimes/dojma/HandleFirebaseMessages.java
@@ -10,33 +10,47 @@ import android.support.v4.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+
 public class HandleFirebaseMessages extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, HomeActivity.class);
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(this, 69, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.dojma))
                         .setSmallIcon(R.drawable.dojma)
                         .setContentTitle(remoteMessage.getNotification().getTitle())
-                        .setContentText(remoteMessage.getNotification().getBody());
-        // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, HomeActivity.class);
-        PendingIntent resultPendingIntent =
-                PendingIntent.getActivity(this, 69, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-        mBuilder.setContentIntent(resultPendingIntent).setAutoCancel(true);
+                        .setContentText(remoteMessage.getNotification().getBody())
+                        .setAutoCancel(true)
+                        .setContentIntent(resultPendingIntent);
+
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         // mId allows you to update the notification later on.
-        try {
-            mBuilder.setStyle(new NotificationCompat.InboxStyle().setSummaryText(remoteMessage
-                    .getData().get
-                            ("extra")));
-        } catch (Exception e) {
-            mBuilder.setStyle(new NotificationCompat.InboxStyle().setSummaryText(remoteMessage
-                    .getNotification().getBody()));
 
+        Map<String, String> map = remoteMessage.getData();
+        String summary = "DoJMA update";
+        if (map.containsKey("summary")) {
+            summary = map.get("summary");
         }
+
+        if (map.containsKey("extra")) {
+            mBuilder.setStyle(new NotificationCompat
+                    .BigTextStyle()
+                    .bigText(map.get("extra"))
+                    .setSummaryText(summary));
+        } else if (map.containsKey("extras")) {
+            mBuilder.setStyle(new NotificationCompat
+                    .BigTextStyle()
+                    .bigText(map.get("extras"))
+                    .setSummaryText(summary));
+        }
+
         mNotificationManager.notify(6969, mBuilder.build());
     }
 }
