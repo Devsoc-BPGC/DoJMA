fbe2155cc1dea8f630b2479c0c7c739563e585b8
Rushikesh Jogdand <rushikeshjogdand1@gmail.com>
2018-06-02 22:36:01 +0530

    fixed journalist build problems

--- journalists/app/src/main/java/com/csatimes/dojmajournalists/MainActivity.java
+++ journalists/app/src/main/java/com/csatimes/dojmajournalists/MainActivity.java
@@ -1,12 +1,14 @@
 package com.csatimes.dojmajournalists;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
