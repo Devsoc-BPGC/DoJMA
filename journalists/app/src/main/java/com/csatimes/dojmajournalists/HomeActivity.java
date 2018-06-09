6e02f52abcf73e2018ad2b8b681953985c898e5f
Harsh Raj <hraj8675@gmail.com>
2018-06-10 00:45:29 +0530

    codacy issues resolved

--- journalists/app/src/main/java/com/csatimes/dojmajournalists/HomeActivity.java
+++ journalists/app/src/main/java/com/csatimes/dojmajournalists/HomeActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
