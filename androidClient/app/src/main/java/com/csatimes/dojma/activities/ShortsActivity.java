6be7a518bfcf8aa8b50ed2849babfe18363aa773
YashxD <31652186+YashxD@users.noreply.github.com>
2018-06-09 16:12:35 +0530

    Issue #87 CampusWatch implemented  (#109)
    * CampusWatch functional.
    Fresco implemented to load Image in the same.

---
+++ androidClient/app/src/main/java/com/csatimes/dojma/activities/ShortsActivity.java
@@ -0,0 +1,25 @@
+package com.csatimes.dojma.activities;
+
+import android.os.Bundle;
+
+import com.csatimes.dojma.R;
+import com.csatimes.dojma.adapters.ShortsVerticalViewPagerAdapter;
+import com.csatimes.dojma.viewholders.ShortsVerticalViewPager;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShortsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shorts);
+        initSwipePager();
+    }
+
+    private void initSwipePager() {
+        ShortsVerticalViewPager shortsVerticalViewPager = (ShortsVerticalViewPager) findViewById(R.id.viewpager_shorts);
+        shortsVerticalViewPager.setAdapter(new ShortsVerticalViewPagerAdapter(this));
+    }
+}
---
+++ androidClient/app/src/main/java/com/csatimes/dojma/models/ShortsItem.java
@@ -0,0 +1,51 @@
+package com.csatimes.dojma.models;
+
+ public class ShortsItem {
+    public String title;
+    public String subtitle;
+    public String timestamp;
+    public String imageUrl;
+
+    public ShortsItem(){
+        //Default Constructor
+    }
+
+    public ShortsItem(String title, String subtitle, String imageUrl, String timestamp) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.timestamp = timestamp;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
